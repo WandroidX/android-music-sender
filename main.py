553c1fb37ee73842f4fed8f46e110269e83d4023
God Wandroid <cristowanderson@gmail.com>
2022-08-09 18:20:33 -0400

    Now you can use wireless adb to send your files.

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
+from typing import Pattern
 from src import adb
 from src import list_music
 import config
 import sys, re
 
 def main():
-    IP: str = config.ANDROID_IP
-    if len(sys.argv) == 2:
-        re_ip = re.compile(r'\d{3}\.\d{3}\.\d\.\d{2}')
-        if re_ip.search(sys.argv[2]):
-            IP: str = sys.argv[2]
+    android_ip: str = config.ANDROID_IP
+    if len(sys.argv) > 1:
+        
+        have_wireless_option: bool = sys.argv[1] == '-w' or sys.argv[1] == '--wireless'
+        if have_wireless_option:
+            if len(sys.argv) != 3:
+                raise Exception('MUST SPECIFY AN IP')
+            IP_INDEX: int = 2
+            re_ip: Pattern = re.compile(r'\d{3}\.\d{3}\.\d+\.\d+:\d{5}')
+            is_valid_ip: bool = bool(re_ip.search( sys.argv[IP_INDEX] ))
         else:
-            raise Exception(f'el argumento "{sys.argv[2]}" no es una ip.')
+            IP_INDEX: int = 1
+            re_ip = re.compile(r'\d{3}\.\d{3}\.\d+\.\d+')
+            is_valid_ip = bool(re_ip.search( sys.argv[IP_INDEX] ))
+
+        if is_valid_ip:
+            android_ip = sys.argv[IP_INDEX]
+        else:
+            raise Exception(f'el argumento "{sys.argv[IP_INDEX]}" no es una ip válida.')
 
     
     musics_in_folder: list[str] = list_music.get_musics_name(config.PC_MUSIC_FOLDER_PATH)
-    adb.connect_to_device(config.ADB_EXECUTABLE_PATH, IP)
+    adb.connect_to_device(config.ADB_EXECUTABLE_PATH, android_ip)
     files_in_device: list[str] = adb.get_device_filenames(
         config.ADB_EXECUTABLE_PATH, config.ANDROID_MUSIC_FOLDER_PATH
     )
@@ -30,3 +43,4 @@ if __name__ == '__main__':
     main()
 else:
     print('Debe ser ejecutado como script')
+
